7fc8d6d8b57342046169bf526f83a1614b6705ca
Phat Pham <101293853+Pham-P7@users.noreply.github.com>
2022-12-05 17:46:14 -0600

    Add files via upload

---
+++ wDs.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class wDs {
+    public static void main(String args[]){
+        String arr[] = {"monday","tuesday","wednesday","thursday","friday","saturday","sunday"};
+        Collections.shuffle(Arrays.asList(arr));
+    }
+}
