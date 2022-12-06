0afeddc71c9daf28f5a5f92989fa7b2159b942f6
NotOzul <112525558+NotOzul@users.noreply.github.com>
2022-12-06 07:01:41 -0600

    Update wDs.java

--- wDs.java
+++ wDs.java
@@ -3,5 +3,7 @@ public class wDs {
     public static void main(String args[]){
         String arr[] = {"monday","tuesday","wednesday","thursday","friday","saturday","sunday"};
         Collections.shuffle(Arrays.asList(arr));
+        arr[0]="monday";arr[1]="tuesday";arr[2]="wedsnesday";arr[3]="thursday";arr[4]="friday";arr[5]="saturday";arr[6]="sunday";
+        System.out.print(String.join(" ",arr));
     }
 }
